5cd31a9168f7dcd733c078c1349e820904505f46
ganesh saini <ganesh@smartseotech>
2024-04-23 13:38:48 +0530

    add files

---
+++ main.js
@@ -0,0 +1,20 @@
+
+/// hameburger"-menu js
+const hameburger=document.querySelector(".hameburger");
+const navList =document.querySelector(".nav-list-ul")
+
+hameburger.addEventListener('click',()=>{
+    hameburger.classList.toggle("active")
+    navList.classList.toggle("active")
+})
+/// hameburger"-menu js
+
+
+let accordian=document.getElementsByClassName("aside-in")
+let i;
+ for (let i = 0; i < array.length; i++) {
+    accordian[i].addEventListener('click', function(){
+
+    })
+    
+ }
